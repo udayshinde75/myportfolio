c6df99061e0ba021c7e90c956c278c7306568258
udayshinde75 <dadashinde247@gmailcom>
2025-04-14 11:54:44 +0530

    ..

--- app/api/dashboard/education/[educationId]/route.ts
+++ app/api/dashboard/education/[educationId]/route.ts
@@ -23,21 +23,19 @@ const EducationSchema = z.object({
 const JWT_SECRET = process.env.JWT_SECRET || "";
 
 export async function GET(
-    _req: NextRequest,
-    context: { params: { educationId: string } }
+    request: NextRequest,
+    { params }: { params: { educationId: string } }
 ) {
     try {
         await connectToDB();
         const token = await getToken();
 
         if (!token) {
-            return NextResponse.json({error:"Unauthorized"}, {status: 401});
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         }
 
         const decoded = jwt.verify(token, JWT_SECRET);
-        const { educationId } = context.params;
-
-        console.log("education ID:",educationId)
+        const { educationId } = params;
 
         if (typeof decoded !== "string" && "userId" in decoded) {
             const education = await Education.findOne({
@@ -48,8 +46,6 @@ export async function GET(
             if (!education) {
                 return NextResponse.json({ error: "Degree not found" }, { status: 404 });
             }
-
-            //console.log(job)
 
             return NextResponse.json({
                 _id: education._id,
@@ -66,10 +62,10 @@ export async function GET(
                 score: education.score,
                 scoreType: education.scoreType,
             });
-            
         }
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     } catch (err) {
-        console.log("GET /api/dashboard/education/[educationId] error", err)
+        console.log("GET /api/dashboard/education/[educationId] error", err);
         return NextResponse.json(
             { error: "Internal Server Error" },
             { status: 500 }
@@ -77,51 +73,47 @@ export async function GET(
     }
 }
 
-export async function PATCH (
-    req: Request,
-    { params } : { params: { educationId: string } }
+export async function PATCH(
+    request: NextRequest,
+    { params }: { params: { educationId: string } }
 ) {
     try {
         await connectToDB();
         const token = await getToken();
 
         if (!token) {
-            return NextResponse.json({error:"Unauthorized"}, {status: 401});
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         }
 
         const decoded = jwt.verify(token, JWT_SECRET);
-        const { educationId } = await params;
+        const { educationId } = params;
 
         if (typeof decoded !== "string" && "userId" in decoded) {
-            const body = await req.json();
+            const body = await request.json();
             const parse = EducationSchema.safeParse(body);
 
-            console.log("Body before parse: ", body)
-
-            console.log("Parsed Data: ", {...parse.data})
-
+            if (!parse.success) {
+                return NextResponse.json({ error: parse.error }, { status: 400 });
+            }
 
             const updatedEducation = await Education.findOneAndUpdate(
                 {
-                  _id: educationId,
-                  user: decoded.userId,
+                    _id: educationId,
+                    user: decoded.userId,
                 },
-                { ...parse.data },
+                parse.data,
                 { new: true }
             );
 
-            console.log(updatedEducation)
-
             if (!updatedEducation) {
-                return NextResponse.json({ error: "Job not found or not authorized to edit." }, { status: 404 });
+                return NextResponse.json({ error: "Education not found or not authorized to edit." }, { status: 404 });
             }
 
             return NextResponse.json({ success: "Education Details updated successfully" });
         }
-        return NextResponse.json({error:"Unauthorized"}, {status: 401});
-        
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     } catch (err) {
-        console.log("PATCH /api/dashboard/Education/[educationId] error", err)
+        console.log("PATCH /api/dashboard/Education/[educationId] error", err);
         return NextResponse.json(
             { error: "Internal Server Error" },
             { status: 500 }
@@ -129,20 +121,20 @@ export async function PATCH (
     }
 }
 
-export async function DELETE (
-    req : Request,
-    { params }: { params: { educationId : string}}
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: { educationId: string } }
 ) {
     try {
         await connectToDB();
         const token = await getToken();
 
         if (!token) {
-            return NextResponse.json({error:"Unauthorized"}, {status: 401});
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         }
 
         const decoded = jwt.verify(token, JWT_SECRET);
-        const {educationId} = await params;
+        const { educationId } = params;
 
         if (typeof decoded !== "string" && "userId" in decoded) {
             const education = await Education.findOne({
@@ -151,16 +143,16 @@ export async function DELETE (
             });
         
             if (!education) {
-            return NextResponse.json({ error: "Job not found or not authorized to delete." }, { status: 404 });
+                return NextResponse.json({ error: "Education not found or not authorized to delete." }, { status: 404 });
             }
 
             await education.deleteOne();
 
-            return NextResponse.json({ success: "Job deleted successfully." }, { status: 200 });
+            return NextResponse.json({ success: "Education deleted successfully." }, { status: 200 });
         }
-        return NextResponse.json({error:"Unauthorized"}, {status: 401});
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     } catch (err) {
-        console.log("DELETE /api/dashboard/education/[educationId] error", err)
+        console.log("DELETE /api/dashboard/education/[educationId] error", err);
         return NextResponse.json(
             { error: "Internal Server Error" },
             { status: 500 }
