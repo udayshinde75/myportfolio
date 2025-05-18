8c3664cd3a2a39df8c7a28b9fe82ced949d407df
udayshinde75 <dadashinde247@gmailcom>
2025-05-18 18:52:16 +0530

    solved error for projectId parameter

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,6 +1,6 @@
 import ProjectInfo from "@/components/custom/projects/project-info";
 
-export default async function ProjectSection({ params }: { params: { projectId: string } }) {
+export default async function ProjectSection({ params }: { params: Promise<{ projectId: string }> }) {
   const {projectId} = await params;
   return <ProjectInfo id={projectId} />;
 }
